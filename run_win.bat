9d54e22715f55ec31dcf1d64c5a9ed457c9252a3
Lunu Bounir <lunu.bounir@gmail.com>
2020-10-24 15:12:54 +0200

    testing win #n

--- run_win.bat
+++ run_win.bat
@@ -3,7 +3,7 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Too
 git clone --branch "v1.2" https://github.com/dacap/clip
 
 cd clip
-cmake -DCMAKE_C_FLAGS:STRING="/MT /O2 /Ob2 /D NDEBUG" -DCMAKE_CXX_FLAGS:STRING="/MT /O2 /Ob2 /D NDEBUG" .
+cmake -DCMAKE_C_FLAGS:STRING="-MT -O2 -Ob2 -D NDEBUG" -DCMAKE_CXX_FLAGS:STRING="-MT -O2 -Ob2 -D NDEBUG" .
 cmake --build . --target clip --config Release
 dir
 cd ..
@@ -11,8 +11,8 @@ cd ..
 wget https://github.com/nlohmann/json/releases/download/v3.9.1/json.hpp
 
 copy helper.mm helper.cpp
-cl.exe /D NDEBUG /c /W3 /WX- /EHsc /MD helper.cpp
-cl.exe /D NDEBUG /c /W3 /WX- /EHsc /MD extra/win.cpp
+cl.exe /D NDEBUG /c /W3 /WX- /EHsc /MT helper.cpp
+cl.exe /D NDEBUG /c /W3 /WX- /EHsc /MT extra/win.cpp
 
 link.exe /OUT:"helper.exe" .\clip\Release\clip.lib user32.lib gdi32.lib /SUBSYSTEM:CONSOLE /DEBUG:NONE /machine:X86 win.obj helper.obj
 
