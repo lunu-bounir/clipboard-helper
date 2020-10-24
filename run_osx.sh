0154da23c7b0eeb36f6df62da0f279820050aa79
Lunu Bounir <lunu.bounir@gmail.com>
2020-10-24 20:16:22 +0200

    d

--- run_osx.sh
+++ run_osx.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-git clone --branch "v1.2" https://github.com/dacap/clip
-pushd clip
-cmake .
-cmake --build . --target clip --config Release
-popd
-wget https://github.com/nlohmann/json/releases/download/v3.9.1/json.hpp -c
-
-g++ helper.mm extra/osx.mm ./clip/libclip.a \
+g++ helper.mm extra/osx.mm \
   -stdlib=libc++ \
   -std=gnu++11 \
   -fobjc-arc \
   -framework AppKit \
-  -I ./clip \
   -o helper
 
 zip  -j -9 mac.zip helper installer/mac/install.sh installer/mac/uninstall.sh
--- run_win.bat
+++ run_win.bat
@@ -1,18 +1,11 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat"
 
-git clone --branch "v1.2" https://github.com/dacap/clip
-
-cd clip
-  cmake -D "CMAKE_C_FLAGS:STRING=/D NDEBUG /c /W3 /WX- /EHsc /MT" -D "CMAKE_CXX_FLAGS:STRING=/D NDEBUG /c /W3 /WX- /EHsc /MT" .
-  cmake --build . --target clip --config Release
-cd ..
-
 wget https://github.com/nlohmann/json/releases/download/v3.9.1/json.hpp
 
 copy helper.mm helper.cpp
 cl.exe /D NDEBUG /c /W3 /WX- /EHsc /MT helper.cpp
 cl.exe /D NDEBUG /c /W3 /WX- /EHsc /MT extra/win.cpp
 
-link.exe /OUT:"helper.exe" .\clip\Release\clip.lib user32.lib gdi32.lib /SUBSYSTEM:CONSOLE /DEBUG:NONE /machine:X86 win.obj helper.obj
+link.exe /OUT:"helper.exe" user32.lib gdi32.lib /SUBSYSTEM:CONSOLE /DEBUG:NONE /machine:X86 win.obj helper.obj
 
 7z a -tzip windows.zip helper.exe .\installer\win\install.bat .\installer\win\uninstall.bat
