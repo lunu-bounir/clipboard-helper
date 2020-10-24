465c4dd55a0818ef62c15d1a34a7173dfaf8023a
Lunu Bounir <lunu.bounir@gmail.com>
2020-10-24 15:25:48 +0200

    testing win #n

--- run_win.bat
+++ run_win.bat
@@ -3,12 +3,6 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Too
 git clone --branch "v1.2" https://github.com/dacap/clip
 
 cd clip
-cmake -DCMAKE_C_FLAGS:STRING="-MV -D NDEBUG" -DCMAKE_CXX_FLAGS:STRING="-MT -D NDEBUG" -DCMAKE_BUILD_TYPE=Release .
-
-
-
-
-
   cmake -D "CMAKE_C_FLAGS_DEBUG_INIT=/D_DEBUG /MTd /Zi /Ob0 /Od /RTC1"\
         -D "CMAKE_C_FLAGS_MINSIZEREL_INIT=/MT /O1 /Ob1 /D NDEBUG"\
         -D "CMAKE_C_FLAGS_RELEASE_INIT=/MT /O2 /Ob2 /D NDEBUG"\
@@ -16,7 +10,8 @@ cmake -DCMAKE_C_FLAGS:STRING="-MV -D NDEBUG" -DCMAKE_CXX_FLAGS:STRING="-MT -D ND
         -D "CMAKE_CXX_FLAGS_DEBUG_INIT=/D_DEBUG /MTd /Zi /Ob0 /Od /RTC1"\
         -D "CMAKE_CXX_FLAGS_MINSIZEREL_INIT=/MT /O1 /Ob1 /D NDEBUG"\
         -D "CMAKE_CXX_FLAGS_RELEASE_INIT=/MT /O2 /Ob2 /D NDEBUG"\
-        -D "CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT=/MT /Zi /O2 /Ob1 /D NDEBUG" --build . --target clip --config Release
+        -D "CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT=/MT /Zi /O2 /Ob1 /D NDEBUG" .
+  cmake --build . --target clip --config Release
 cd ..
 
 wget https://github.com/nlohmann/json/releases/download/v3.9.1/json.hpp
