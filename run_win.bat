c7a893a49fa0dda71a210124297758907b51a718
Lunu Bounir <lunu.bounir@gmail.com>
2020-10-24 14:30:13 +0200

    testing win #n

--- run_win.bat
+++ run_win.bat
@@ -3,7 +3,7 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Too
 git clone --branch "v1.2" https://github.com/dacap/clip
 
 cd clip
-cmake -D LLVM_USE_CRT_RELEASE=MT .
+cmake -D "CMAKE_C_FLAGS:STRING=/MT /O2 /Ob2 /D NDEBUG" -D "CMAKE_CXX_FLAGS:STRING=/MT /O2 /Ob2 /D NDEBUG" .
 cmake --build . --target clip --config Release
 
 dir
